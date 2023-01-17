ccbf8f72d01be241b5ecaa7292e3ddf4556aba8c
김연진 <love_homebody@naver.com>
2023-01-18 00:07:10 +0900

    Docs: 주석 추가

--- src/api/feed/controller.js
+++ src/api/feed/controller.js
@@ -1,11 +1,13 @@
 const model = require('./query');
 
 exports.index = async (ctx, next) => {
+    // 전체 글 조회
     let result = await model.getAllFeed();
     ctx.response.body = result;
 }
 
 exports.store = async (ctx, next) => {
+    // 글 작성 모듈
     let user_id = ctx.request.decoded.id
     let { image_id, content } = ctx.request.body;
     let { affectedRows, insertId } = await model.create(user_id, image_id, content);
@@ -21,16 +23,18 @@ exports.store = async (ctx, next) => {
 }
 
 exports.show = async (ctx, next) => {
+    // 특정 글 조회 모듈
     let id = ctx.request.params.id;
-    let item = await model.show(id);
+    let item = await model.getFeed(id);
     if (item != null) {
         ctx.body = item;
     } else {
-        ctx.body = "해당 게시물은 존재하지 않습니다."
+        ctx.body = "해당 게시물은 존재하지 않습니다.";
     }
 }
 
 exports.update = async (ctx, next) => {
+    // 글 수정 모듈
     let id = ctx.params.id;
     let { content } = ctx.request.body;
     let { affectedRows } = await model.update(id, content);
@@ -43,6 +47,7 @@ exports.update = async (ctx, next) => {
 }
 
 exports.delete = async (ctx, next) => {
+    //글 삭제 모듈
     let id = ctx.params.id;
     let { affectedRows } = await model.delete(id);
     let result = "";
--- src/api/feed/query.js
+++ src/api/feed/query.js
@@ -1,24 +1,46 @@
 const {pool} = require('../../data')
 
-exports.create = async (user, image, content) => {
+/**
+ * 글을 등록하면 DB에 등록한다.
+ * @param {number} user_id
+ * @param {number} image_id 
+ * @param {String} content 글 내용
+ * @returns 
+ */
+exports.create = async (user_id, image_id, content) => {
     const query = `INSERT INTO feed
     (user_id, image_id, content) 
     VALUES (?, ?, ?)`;
-    return await pool(query, [user, image, content]);
+    return await pool(query, [user_id, image_id, content]);
 }
 
-exports.show = async (id) => {
+/**
+ * 특정 글이 존재하는지 확인하고 글이 존재할 경우 내용을 불러온다.
+ * @param {number} id 
+ * @returns 
+ */
+exports.getFeed = async (id) => {
     const query = `SELECT * FROM feed WHERE id = ?`;
     let result = await pool(query, [id]);
     return (result.length < 0) ? null : result[0];
 }
 
+/**
+ * 등록되어 있는 모든 글을 불러온다.
+ * @returns 
+ */
 exports.getAllFeed = async () => {
     const query = `select * from feed`;
     let result = await pool(query);
     return result;
 }
 
+/**
+ * 선택한 글의 내용을 변경한다.
+ * @param {number} id feed의 id (기본키)
+ * @param {String} modifications 수정할 내용
+ * @returns 
+ */
 exports.update = async (id, modifications) => {
     const query = `UPDATE feed
     SET content = ?
@@ -26,6 +48,11 @@ exports.update = async (id, modifications) => {
     return await pool(query, [modifications, id]);
 }
 
+/**
+ * 선택한 글을 삭제한다.
+ * @param {number} id 
+ * @returns 
+ */
 exports.delete = async (id) => {
     const query = `DELETE FROM feed
     WHERE id = ?`;
