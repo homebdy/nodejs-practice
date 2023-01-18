b4e46ee3fcfa14f2f618b974a9cd134a98c266e6
김연진 <love_homebody@naver.com>
2023-01-18 12:29:29 +0900

    fix: jwt verify

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,21 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 exports.verify = async (ctx, next) => {
-    try {
-		ctx.request.decoded = jwt.verify(ctx.request.headers.token, process.env.APP_KEY);
-		return next();
-	} catch (error) {
-		if (error.name === 'TokenExpiredError') {
-	  		ctx.status = 419;
-            ctx.body = {
-                "message":'토큰 만료'
-            }
-            return;
-		}
-        ctx.status = 401
-        ctx.body = {
-            "message":'유효하지 않은 토큰'
+    var token = ctx.request.headers['token'];
+    await jwt.verify(token, process.env.APP_KEY, async (error, decoded) => {
+        if(error) {
+            ctx.body = "에러";
+            return
         }
-        return;
-	}
+        ctx.request.decoded = decoded;
+        await next();
+    })
 }
